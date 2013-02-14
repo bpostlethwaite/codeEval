1a01ffbc6a880b7683320d4ca26b86c5897a7d76
Ben Postlethwaite <post.ben.here@gmail.com>
2013-02-14 13:44:13 -0800

    add problem description

--- decryption_BitTorrent.js
+++ decryption_BitTorrent.js
@@ -7,6 +7,21 @@
  * 2013
  */
 
+/*
+For this challenge you are given an encrypted message and a key. You have to determine the encryption and encoding technique and then print out the corresponding plaintext message. You can assume that the plaintext corresponding to this message, and all messages you must handle, will be comprised of only the characters A-Z and spaces; no digits or punctuation.
+
+Input sample:
+
+There is no input for this program. The encrypted message and key is:
+
+message: "012222 1114142503 0313012513 03141418192102 0113 2419182119021713 06131715070119",
+keyed_alphabet: "BHISOECRTMGWYVALUZDNFJKPQX"
+Output sample:
+
+Print out the plaintext message. (in CAPS)
+*/
+
+
 function decode (msg) {
   var key =  "BHISOECRTMGWYVALUZDNFJKPQX"
   var spacer = "A".charCodeAt(0)
--- robotMovements_TrialPay.c
+++ robotMovements_TrialPay.c
@@ -7,6 +7,20 @@
  * 2013
  */
 
+/*
+Description:
+
+A robot is located at the top-left corner of a 4x4 grid. The robot can move either up, down, left, or right, but can not visit the same spot twice. The robot is trying to reach the bottom-right corner of the grid.
+
+Input sample:
+
+There is no input for this program.
+
+Output sample:
+
+Print out the unique number of ways the robot can reach its destination. (The number should be printed as an integer whole number eg. if the answer is 10 (its not !!), print out 10, not 10.0 or 10.00 etc)
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
