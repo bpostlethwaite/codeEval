6dfa4060852bd3fb3ecc2b218b101a89825ae0fa
Ben Postlethwaite <post.ben.here@gmail.com>
2013-07-21 20:11:59 -0700

    auto input

---
+++ genInput.js
@@ -0,0 +1,21 @@
+var p = require("printarray");
+var at = require("arraytools");
+
+
+var nx = 6
+var ny = 5
+
+var a = []
+
+for (var i = 1; i <= nx*ny; i++)
+  a[i-1] = i
+
+
+if (process.argv[2] === "-p")
+  p(a, 4, [nx, ny])
+
+if (process.argv[2] === "-w") {
+  process.stdout.write(nx + ";" + ny + ";")
+  process.stdout.write(a.join(" "))
+  process.stdout.write("\n")
+}
